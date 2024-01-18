2dd1033cea37303c472528bfefd217e700cf2081
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-01-18 19:57:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Confetti from 'react-confetti';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Confetti />
+      <h1>Planet, calling Greenfield..</h1>
+        <p>Beep Beep Boop Boop</p>
     </div>
   );
 }
